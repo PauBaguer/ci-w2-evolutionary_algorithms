959bd3f9ef7a2ec0096c1be703fe58073da35ef3
msuarf02 <55186340+msuarf02@users.noreply.github.com>
2023-12-26 18:13:12 +0100

    Ajustes ES_cma

--- ES_cma.py
+++ ES_cma.py
@@ -12,50 +12,60 @@ from cma import CMA
 def predict(model, input):
     return model(input)
 
+def obtain_mse(individual, X, y):
+	weights = []
+	start_idx = 0
+	for layer in model.layers:
+		num_params = np.prod(layer.get_weights()[0].shape)
+		
+		layer_weights = tf.reshape(individual[start_idx:start_idx + num_params], (layer.get_weights()[0].shape))
+		layer.set_weights([layer_weights])
+
+		weights.extend(layer.get_weights()[0].flatten())
+		start_idx += num_params
+
+	predictions = predict(model, X)
+	mse = mean_squared_error(y, predictions)
+	return mse
+	
+
 # Create a fitness function to minimize mean squared error
 def fitness_function(individuals):
-	global n_neurons, X_train, y_train
-	model = Sequential()
-	model.add(Dense(n_neurons, input_dim=X_train.shape[1], activation='relu', use_bias=False))
-	model.add(Dense(1, activation='linear', use_bias=False))
-
+	global n_neurons
 	mses = []
-    # Set the weights of the model
 	for individual in individuals:
-		weights = []
-		start_idx = 0
-		for layer in model.layers:
-			num_params = np.prod(layer.get_weights()[0].shape)
-			
-			layer_weights = tf.reshape(individual[start_idx:start_idx + num_params], (layer.get_weights()[0].shape))
-			layer.set_weights([layer_weights])
-
-			weights.extend(layer.get_weights()[0].flatten())
-			start_idx += num_params
-
-		predictions = predict(model, X_train)
-		mse = mean_squared_error(y_train, predictions)
-		mses.append(mse)
+		mses.append(obtain_mse(individual, X_train, y_train))
 	return tf.constant(mses)
 
-max_epochs = 500
+max_epochs = 50
+history = {
+      'best_train': [],
+	  'best_test': []
+}
 def logging_function(cma, logger):
-    if cma.generation % 10 == 0:
-        fitness = cma.best_fitness()
-        logger.info(f'Generation {cma.generation} - fitness {fitness}')
-        print(f'Generation {cma.generation} - fitness {fitness}')
+	fitness = cma.best_fitness()
+	history['best_train'].append(fitness)
+	sol = cma.best_solution()
+	history['best_test'].append(obtain_mse(sol, X_test, y_test))
 
-    if cma.termination_criterion_met or cma.generation == max_epochs:
-        sol = cma.best_solution()
-        fitness = cma.best_fitness()
-        logger.info(f'Final solution at gen {cma.generation}: {sol} (fitness: {fitness})')
-        print(f'Final solution at gen {cma.generation}: {sol} (fitness: {fitness})')
+	if cma.generation % 10 == 0:
+		logger.info(f'Generation {cma.generation} - fitness {fitness}')
+		print(f'Generation {cma.generation} - fitness {fitness}')
+
+	if cma.termination_criterion_met or cma.generation == max_epochs:
+		sol = cma.best_solution()
+		logger.info(f'Final solution at gen {cma.generation}: {sol} (fitness: {fitness})')
+		print(f'Final solution at gen {cma.generation}: {sol} (fitness: {fitness})')
 
 np.random.seed(123)
 X_train, X_test, y_train, y_test = data.generate_synthetic_data_reg(200, 0.01)
 
 n_neurons = 10
 n_weights = X_train.shape[1] * n_neurons + n_neurons
+
+model = Sequential()
+model.add(Dense(n_neurons, input_dim=X_train.shape[1], activation='relu', use_bias=False))
+model.add(Dense(1, activation='linear', use_bias=False))
 
 cma = CMA(
     initial_solution=np.random.uniform(-1, 1, n_weights),
@@ -66,5 +76,12 @@ cma = CMA(
 
 print('Starting CMA-ES')
 with tf.device('/GPU:0'):
-	best_solution, best_fitness = cma.search()
+	best_solution, best_fitness = cma.search(max_epochs)
 print('CMA-ES completed')
+
+plt.plot(history['best_train'])
+plt.plot(history['best_test'])
+plt.xlabel('Generation')
+plt.ylabel('Fitness')
+plt.legend(['Training', 'Validation'])
+plt.savefig('cma.png')
